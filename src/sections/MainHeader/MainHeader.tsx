e2ba12d67c4d7b6ac20dafb3924b78af2acadd47
Kamil <szostekkamil19@gmail.com>
2023-01-27 20:48:20 +0100

    fixing bugs

--- src/sections/MainHeader/MainHeader.tsx
+++ src/sections/MainHeader/MainHeader.tsx
@@ -4,13 +4,19 @@ import HeaderText from '../../components/HeaderText/HeaderText';
 import logo from '../../assets/header/logo-color.png';
 import './MainHeader.scss';
 
-
 const MainHeader: React.FunctionComponent = () => {
     return (
         <section className='main-header'>
             <img src={logo} alt="Logo siłowni power gym" />
             <Navigation />
             <HeaderText />
+            <div className='bg-images'>
+                <div className='bg-image' />
+                <div className='bg-image' />
+                <div className='bg-image' />
+                <div className='bg-image' />
+                <div className='bg-image' />
+            </div>
         </section>
     );
 };
