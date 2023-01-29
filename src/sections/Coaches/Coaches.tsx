d1dadf1544ece5f9e8ccc8a28875fb14cc120865
Kamil <szostekkamil19@gmail.com>
2023-01-29 09:56:36 +0100

    loose

--- src/sections/Coaches/Coaches.tsx
+++ src/sections/Coaches/Coaches.tsx
@@ -14,11 +14,19 @@ const Coaches: React.FunctionComponent<ICoachesProps> = ({ coachesFiltered }) =>
   const dotsRef = useRef<HTMLDivElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
 
+  const slideToCard = (e: React.MouseEvent<HTMLDivElement>) => {
+    const wrapper = wrapperRef.current!;
+    const transformX = parseInt(window.getComputedStyle(wrapper).getPropertyValue("transform").split(',')[4]);
+    const id = parseInt(e.currentTarget.id);
+    const length = e.currentTarget.parentNode?.childNodes.length!;
+    wrapper.style.transform = `translateX(${wrapper.offsetWidth * (id / length)}px)`
+  }
+
   const coachesElement = coachesFiltered
     ? ([...coachesFiltered, ...coachesFiltered]?.map((coach, key) => (
       <CoachCard
         key={useId()}
-        index={key+1}
+        index={key + 1}
         id={coach.id}
         name={coach.name}
         img={coach.image}
@@ -30,7 +38,7 @@ const Coaches: React.FunctionComponent<ICoachesProps> = ({ coachesFiltered }) =>
     : ([...coaches, ...coaches]?.map((coach, key) => (
       <CoachCard
         key={useId()}
-        index={key+1}
+        index={key + 1}
         id={coach.id}
         name={coach.name}
         img={coach.image}
@@ -42,24 +50,22 @@ const Coaches: React.FunctionComponent<ICoachesProps> = ({ coachesFiltered }) =>
 
   const dotElements = coachesFiltered
     ? (coachesFiltered?.map((coach, key) => {
-      const dotId = `dot${key+1} dot${key+1+coachesFiltered.length}`
+      const dotClass = `dot${key + 1} dot${key + 1 + coachesFiltered.length}`
       return (
-        <div key={useId()} id={dotId} className="dot" />)
+        <div key={useId()} style={{minWidth: '10px', backgroundColor: 'rgba(0, 0, 0, 0.5)', borderRadius: '50%', transition: '0.5s'}} id={(key + 1).toString()} className={dotClass} onClick={slideToCard} />)
     }))
     : (coaches?.map((coach, key) => {
-      const dotClass = `dot dot${key+1} dot${key+1+coaches.length}`
+      const dotClass = `dot dot${key + 1} dot${key + 1 + coaches.length}`
       return (
-        <div key={useId()} className={dotClass} />)
+        <div key={useId()} id={(key + 1).toString()} className={dotClass} onClick={slideToCard} />)
     }))
 
   return (
     <section id="coaches" className="coaches">
       <SectionTitle title="Nasz skład trenerski" />
-      <div
-        className="coaches__container"
+      <div className="coaches__container"
         onMouseEnter={() => wrapperRef.current?.classList.add('paused')}
-        onMouseLeave={() => wrapperRef.current?.classList.remove('paused')}
-      >
+        onMouseLeave={() => wrapperRef.current?.classList.remove('paused')}>
         <div className="wrapper" ref={wrapperRef}>
           {coachesElement}
         </div>
